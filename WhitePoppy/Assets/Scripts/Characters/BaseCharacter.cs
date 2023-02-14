ab33e66e9f12b41913dc904e6c7f2490d5e97e29
NeneWolf <ineslobo@hotmail.com>
2023-02-14 05:24:26 +0000

    Player Update
    - Input Manager
    - Base Character Movement ( Movement, jump, sprint, crouch)
    - Added Cinemachine ( Main & Pick Up )
    - Cinemachine Animation
    - Input Binding
    - Pick up body and drop area

--- WhitePoppy/Assets/Scripts/Characters/BaseCharacter.cs
+++ WhitePoppy/Assets/Scripts/Characters/BaseCharacter.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BaseCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private InputManager inputManager;
+
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float sprintSpeed = 4.0f;
+    [SerializeField]
+    private float crouchSpeed = 1.0f;
+    [SerializeField]
+    private float cameraRotationSpeed = 3f;
+
+    private float gravityValue = -9.81f;
+
+    private Transform cameraTransform;
+    
+    public bool isMoving = true;
+    public bool isCarrying = false;
+    #endregion
+
+    private void Start()
     {
-        
+        controller = gameObject.GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+        cameraTransform = Camera.main.transform;
+
+        // Turn off Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isMoving)
+        {
+            Movement();
+            Jump();
+        }
+    }
+
+    void Movement()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0f;
         
+        if(inputManager.IsSprinting() && !isCarrying)
+            controller.Move(move * sprintSpeed * Time.deltaTime); //zoomies
+        else if (inputManager.IsCrouching() && !isCarrying)
+            controller.Move(move * crouchSpeed * Time.deltaTime); // Add change collider
+        else if(isCarrying)
+            controller.Move(move * (playerSpeed*0.6f) * Time.deltaTime); // Reduce speed while carrying "someone"
+        else
+            controller.Move(move * playerSpeed * Time.deltaTime); //Normal speed
+
+        //Rotate towards camera direction
+        float targetAngle = cameraTransform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, cameraRotationSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        // Changes the height position of the player..
+        if (inputManager.PlayerJump() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
---
+++ WhitePoppy/Assets/Scripts/Characters/InputManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    private static InputManager instance;
+
+    //For the player only
+    private bool isSprinting = false;
+    private bool isCrouching = false;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private PlayerInputSystem controls;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        
+        controls = new PlayerInputSystem();
+        InitialiseInput();
+    }
+
+    private void InitialiseInput()
+    {
+        controls.Player.SprintStart.performed += ctx => SprintPressed();
+        controls.Player.SprintFinish.performed += ctx => SprintReleased();
+        controls.Player.CrouchStart.performed += ctx => CrouchPressed();
+        controls.Player.CrouchFinish.performed += ctx => CrouchReleased();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    #region Player Movement
+    public Vector2 GetPlayerMovement()
+    {
+        return controls.Player.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return controls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerJump()
+    {
+        return controls.Player.Jump.triggered;
+    }
+
+    #region Sprinting
+    public bool IsSprinting()
+    {
+        return isSprinting;
+    }
+
+    private void SprintPressed()
+    {
+        isSprinting = true;
+    }
+
+    private void SprintReleased()
+    {
+        isSprinting = false;
+    }
+    #endregion
+
+    #region Crouching
+    public bool IsCrouching()
+    {
+        return isCrouching;
+    }
+
+    private void CrouchPressed()
+    {
+        isCrouching = true;
+    }
+
+    private void CrouchReleased()
+    {
+        isCrouching = false;
+    }
+    #endregion
+    #endregion
+}
