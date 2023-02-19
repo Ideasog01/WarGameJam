3f3a489ba0c7aa227e3902bbd54db0a8f96a1dc5
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-02-19 13:38:30 +0000

    Revert "Update"
    This reverts commit d5244011cd2d16f2a30475c102502566c9d9ed02.

--- WhitePoppy/Assets/Scripts/Characters/EnemyCharacter.cs
+++ WhitePoppy/Assets/Scripts/Characters/EnemyCharacter.cs
@@ -55,11 +55,7 @@ public class EnemyCharacter : BaseCharacter
     private void Update()
     {
 <<<<<<< HEAD
-<<<<<<< HEAD
         if(Health > 0 && _soldierCharacter.Health > 0 && GameManager.gameInProgress)
-=======
-        if(Health > 0 && _soldierCharacter.Health > 0)
->>>>>>> parent of 9db7fa1 (Last update)
 =======
         if(Health > 0 && _soldierCharacter.Health > 0)
 >>>>>>> parent of 9db7fa1 (Last update)
@@ -125,11 +121,8 @@ public class EnemyCharacter : BaseCharacter
         this.GetComponent<CapsuleCollider>().enabled = false;
         rifleObj.SetActive(false);
 <<<<<<< HEAD
-<<<<<<< HEAD
 
         GameManager.objectiveManager.UpdateObjective(1, Objective.ObjectiveType.DefeatEnemy);
-=======
->>>>>>> parent of 9db7fa1 (Last update)
 =======
 >>>>>>> parent of 9db7fa1 (Last update)
     }
